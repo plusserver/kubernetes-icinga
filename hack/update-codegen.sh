d727a5825cf56e6380a90277c6252eda242e7593
amitbenami <benami.amit1@gmail.com>
2020-08-13 17:15:30 +0300

    Fixing the go module problem

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 go mod vendor
+
 if [ -d "$GOPATH/src/k8s.io/code-generator" ]; then
   CODEGEN_PKG="$GOPATH/src/k8s.io/code-generator"
 fi
@@ -14,7 +15,7 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-ge
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/Soluto-Private/kubernetes-icinga/pkg/client github.com/Soluto-Private/kubernetes-icinga/pkg/apis \
   icinga.nexinto.com:v1 \
-  --output-base "${SCRIPT_ROOT}" \
+  --output-base "${SCRIPT_ROOT}"/../../../ \
   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
 
 find pkg/client -type f -name '*.go' -print0 | xargs -0 sed -i '' 's/github.com\/nexinto/github.com\/Nexinto/g'
