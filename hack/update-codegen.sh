77850993f3d8302e644ee9be97e5422c1b2472b0
amitbenami <benami.amit1@gmail.com>
2020-08-13 16:53:23 +0300

    Testing the scripts

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -11,14 +11,11 @@ fi
 SCRIPT_ROOT=`pwd`/$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-( cd $CODEGEN_PKG
-
-GO111MODULE="off" bash ./generate-groups.sh "deepcopy,client,informer,lister" \
+bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/Soluto-Private/kubernetes-icinga/pkg/client github.com/Soluto-Private/kubernetes-icinga/pkg/apis \
   icinga.nexinto.com:v1 \
+  --output-base "${SCRIPT_ROOT}" \
   --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
-
-)
 
 find pkg/client -type f -name '*.go' -print0 | xargs -0 sed -i '' 's/github.com\/nexinto/github.com\/Nexinto/g'
 
